0ba01d90996b217694cc800ecea5f7caa1b3ef01
John Gionti <jgionti@ufl.edu>
2022-12-12 23:54:41 -0500

    Fixed /whojust and renamed to /vclog Accidentally deleted a helper function the voice event needed...

--- src/cogs/voice.py
+++ src/cogs/voice.py
@@ -20,12 +20,40 @@ class Voice(commands.Cog):
         await self.update_vc_log(member, before, after)
 
     #######################
+    #  HELPER FUNCTIONS   #
+    #######################
+
+    # Update VoiceState changelog for /vclog
+    async def update_vc_log(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        action = ""
+        channel = None
+
+        # Return if a member is a bot
+        if member.bot:
+            return
+
+        # Check for join
+        if after.channel and (not before.channel or before.channel.id != after.channel.id):
+            action = "joined"
+            channel = after.channel
+        # Check for leave
+        elif before.channel and (not after.channel or after.channel.id != before.channel.id):
+            action = "left"
+            channel = before.channel
+
+        # If a check passed, add to log
+        if action != "":
+            log_data = {"member" : member.mention, "action" : action, "channel" : channel.mention, "time" : time.time()}
+            self.vc_log.insert(0, log_data)
+            if (len(self.vc_log) > 10): self.vc_log.pop()
+
+    #######################
     #       COMMANDS      #
     #######################
 
     # Send who just joined or left the vc
     @commands.slash_command(guild_ids = [730196305124655176])
-    async def whojust(self, ctx):
+    async def vclog(self, ctx):
         """Find out who recently joined or left the voice channel."""
         if len(self.vc_log) == 0:
             await ctx.respond("Nobody's done anything recently, bozo! 🤡", ephemeral=True)
